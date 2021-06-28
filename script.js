9ad883d86bf06fab38cebc195728d5855183e1f7
Davide Passerò <shardonian86@gmail.com>
2021-06-28 09:51:01 +0200

    planet tab component functionality

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// The function below will create a new instance of
-// a XMLHttpRequest and load asynchronously the content
-// of data.json
-function loadJSON(callback) {   
-    const request = new XMLHttpRequest();
-    request.overrideMimeType("application/json");
-    request.open('GET', './data.json', true);
-    request.onreadystatechange = function () {
-      if (request.readyState == 4 && request.status == "200") {
-        callback(JSON.parse(request.responseText));
-      }
-    };
-    request.send(null);  
-  }
 
-   function init() {
-    loadJSON(function(response) {   
-       console.log(response);
+// Tab component
+const sliderContainer = document.querySelector('.slider__tab--container');
+const tabs = document.querySelectorAll('.slider__tab');
+const images = document.querySelectorAll('.slider__images');
+const content = document.querySelectorAll('.slider__content');
+
+    sliderContainer.addEventListener('click', function(e) {
+        e.preventDefault();
+        const clicked = e.target;
+       
+        tabs.forEach(t => t.classList.remove('underline__active'));
+        content.forEach(c => c.classList.remove('slider__content--active'));
+        images.forEach(img => img.classList.remove('slider__img--active'));
+    
+        clicked.classList.add('underline__active');
+        
+        document.querySelector(`.slider__content--${clicked.dataset.tab}`).classList.add('slider__content--active');
+        document.querySelector(`.slider__img--${clicked.dataset.tab}`).classList.add('slider__img--active');
     });
-   }
-
- init();
